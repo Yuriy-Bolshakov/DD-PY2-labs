b9eb8e2cc5d292db0b5e92d6dc8d5c53eeb9acfb
Yuriy-Bolshakov <bolshakovyuriy11@gmail.com>
2025-02-10 21:02:13 +0300

    Лабораторная работа 3

--- task1.py
+++ task1.py
@@ -1,189 +1,62 @@
-import doctest
-class x:
-    def __init__(self, views_number: int, likes_number: int):
-        """
-        Создание и подготовка объекта "Соцсеть"
+class Book:
+    def init(self, name: str, author: str):
+        self._name = name
+        self._author = author
 
-        :param views_number: Количество просмотров
-        :param likes_number: Количество лайков
+    @property
+    def name(self):
+        return self._name
 
-        Примеры:
-        >>> x = x(500, 600)  # Инициализация экземпляра класса
-        >>> x.views_number
-        500
-        >>> x.likes_number
-        600
-        """
-        if not isinstance(views_number, int):
-            raise TypeError("Количество просмотров должно быть типа int")
-        if views_number < 0:
-            raise ValueError("Количество просмотров должно быть неотрицательным числом")
-        self.views_number = views_number
+    @property
+    def author(self):
+        return self._author
 
-        if not isinstance(likes_number, int):
-            raise TypeError("Количество лайков должно быть типа int")
-        if likes_number < 0:
-            raise ValueError("Количество лайков должно быть неотрицательным числом")
-        self.likes_number = likes_number
+    def str(self):
+        return f"Книга {self.name}. Автор {self.author}"
 
-    def system_operation(self) -> bool:
-        """
-        Функция, которая проверяет наличие просмотров и лайков
-
-        :return: True, если есть просмотры и лайки, иначе False
-
-        Примеры:
-        >>> x = x(500, 600)
-        >>> x.system_operation()
-        True
-        """
-        return self.views_number > 0 and self.likes_number > 0
-
-    def detection_of_violation(self) -> bool:
-        """
-        Обнаружение обмана программы (накрутка)
-
-        :return: True, если есть факт нарушения, иначе False
-
-        Примеры:
-        >>> x = x(500, 600)
-        >>> x.detection_of_violation()
-        False
-        """
-        return self.likes_number > self.views_number
+    def repr(self):
+        return f"{self.class.name}(name={self.name!r}, author={self.author!r})"
 
 
-class Cistern:
-    def __init__(self, cistern_volume: float, occupied_volume: float):
-        """
-        Создание и подготовка к работе объекта "Цистерна"
+class PaperBook(Book):
+    def init(self, name: str, author: str, pages: int):
+        super().init(name, author)
+        self.pages = pages
 
-        :param cistern_volume: Объем цистерны
-        :param occupied_volume: Объем занимаемого топлива
+    @property
+    def pages(self):
+        return self._pages
 
-        Примеры:
-        >>> cistern = Сistern(100, 50)  # Инициализация экземпляра класса
-        >>> cistern.cistern_volume
-        100
-        >>> cistern.occupied_volume
-        50
-        """
-        if not isinstance(cistern_volume, (int, float)):
-            raise TypeError("Объем цистерны должен быть типа int или float")
-        if cistern_volume <= 0:
-            raise ValueError("Объем топливного бака должен быть положительным")
-        self.cistern_volume = cistern_volume
+    @pages.setter
+    def pages(self, value: int):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество страниц должно быть положительным целым числом.")
+        self._pages = value
 
-        if not isinstance(occupied_volume, (int, float)):
-            raise TypeError("Объем топлива должен быть типа int или float")
-        if occupied_volume < 0:
-            raise ValueError("Количество топлива должно быть неотрицательным")
-        self.occupied_volume = occupied_volume
+    def str(self):
+        return f"Бумажная книга {self.name}. Автор {self.author}. Страниц: {self.pages}"
 
-    def fuel_waste(self, spent_fuel: float) -> None:
-        """
-        Расход бензина из-за отработки
-
-        :param spent_fuel: Объем отработанного бензина
-        :raise ValueError: Если количество отработанного топлива превышает количество топлива в цистерне.
-
-        Примеры:
-        >>> cistern = Сistern(100, 50)
-        >>> cistern.fuel_waste(20)
-        >>> cistern.occupied_volume
-        30
-        >>> cistern.fuel_waste(50)  # Это вызовет ошибку
-        Traceback (most recent call last):
-            ...
-        ValueError: Недостаточно топлива в цистерне
-        """
-        if spent_fuel > self.occupied_volume:
-            raise ValueError("Недостаточно топлива в цистерне")
-        self.occupied_volume -= spent_fuel
-
-    def cistern_filling(self, fuel: float) -> None:
-        """
-        Заправка цистерны
-
-        :param fuel: Количество заправляемого топлива
-        :raise ValueError: Если количество заправляемого топлива превышает свободное место.
-
-        Примеры:
-        >>> cistern = Сistern(100, 50)
-        >>> cistern.cistern_filling(30)  # Успешная заправка
-        >>> cistern.occupied_volume
-        80
-        >>> cistern.cistern_filling(30)  # Это вызовет ошибку
-        Traceback (most recent call last):
-            ...
-        ValueError: Превышен объем
-        """
-        if fuel + self.occupied_volume > self.cistern_volume:
-            raise ValueError("Превышен объем цистерны")
-        self.occupied_volume += fuel
+    def repr(self):
+        return f"{self.class.name}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
 
 
-class wallpaper:
-    def __init__(self, color: str, area: float):
-        """
-        Создание и подготовка к работе объекта "Обои"
+class AudioBook(Book):
+    def init(self, name: str, author: str, duration: float):
+        super().init(name, author)
+        self.duration = duration
 
-        :param color: Цвет
-        :param area: Площадь
+    @property
+    def duration(self):
+        return self._duration
 
-        Примеры:
-        >>> wallpaper = Wallpaper("красный", 100)  # Инициализация экземпляра класса
-        >>> wallpaper.color
-        'красный'
-        >>> wallpaper.area
-        100
-        """
-        if not isinstance(color, str):
-            raise TypeError("Цвет должен быть типа str")
-        self.color = color
+    @duration.setter
+    def duration(self, value: float):
+        if not isinstance(value, float) or value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом с плавающей запятой.")
+        self._duration = value
 
-        if not isinstance(area, (int, float)):
-            raise TypeError("Площадь должна быть типа int или float")
-        if area <= 0:
-            raise ValueError("Площадь должна быть положительным числом")
-        self.area = area
+    def str(self):
+        return f"Аудиокнига {self.name}. Автор {self.author}. Продолжительность: {self.duration} часов"
 
-    def init(self) -> str:
-        """
-        Функция инициализирует обои с заданным цветом
-
-        :raise ValueError: Если строка "Цвет" - пустая.
-
-        Примеры:
-        >>> wallpaper = Wallpaper("red", 100)
-        >>> wallpaper.init()
-        'Инициализированы обои цвета red'
-        """
-        if not self.color:
-            raise ValueError("Цвет не может быть пустым")
-        return f"Инициализированы обои цвета {self.color}"
-
-    def color_change(self, new_color: str) -> None:
-        """
-        Изменение цвета
-
-        :param new_color: Новый цвет
-        :raise ValueError: Если new_color - пустая строка
-
-        Примеры:
-        >>> wallpaper = Wallpaper("red", 100)
-        >>> wallpaper.color_change("blue")
-        >>> wallpaper.color
-        'blue'
-        >>> wallpaper.color_change("")  # Это вызовет ошибку
-        Traceback (most recent call last):
-            ...
-        ValueError: Новый цвет не может быть пустым
-        """
-        if not new_color:
-            raise ValueError("Новый цвет не может быть пустым")
-        self.color = new_color
-
-
-if __name__ == "__main__":
-    doctest.testmod()  # Тестирование примеров, которые находятся в документации
+    def repr(self):
+        return f"{self.class.name}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
